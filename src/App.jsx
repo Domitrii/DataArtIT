3f5cd8863adfb5a8816e2793bc6e3c3a50299688
Domitrii <126395589+Domitrii@users.noreply.github.com>
2025-02-17 01:14:46 +0000

    first try

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { GrFormNext } from "react-icons/gr";
+import s from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className={s.fBody}>
+      <div className={s.topSection}>
+        <h1>RanDay.jokes</h1>
+        <p>Welcome to RanDay.jokes, where we will publish jokes to make you laugh</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className={s.mainSection}>
+        <div className={s.jokeBlock}>
+          Why did the baker go to therapy?
+          Because every time he tried to make dough, he just kept kneading validation from everyone around him!
+        </div>
+        <span className={s.blockBtn}>
+          <p>Next joke</p>
+          <button className={s.nextBtn}><GrFormNext className={s.iconBtn} /></button>
+        </span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </section>
   )
 }
 
