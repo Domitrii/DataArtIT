31f2094689619078407f139d46f5dc7cc069e14b
Domitrii <126395589+Domitrii@users.noreply.github.com>
2025-02-18 00:58:10 +0000

    tap next and go

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
 import { GrFormNext } from "react-icons/gr";
 import s from './App.module.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [header, setHeader] = useState('')
+  const [response, setResponse] = useState('');
+  const [emojis, setEmojis] = useState([])
+  const [emojiRes, setEmojiRes] = useState([])
+  const [numb , setNumb] = useState(0)
+
+  const getData = async () => {
+    try {
+      const result = await axios.get('http://localhost:7070/api/');
+
+      setHeader(result.data.question)
+      setResponse(result.data.answer)
+
+
+      setEmojis(result.data.availableVotes)
+      for(let i = 0; i < 3; i++){
+        emojiRes[i] = 
+        <button 
+        className={s.emoji} 
+        onClick={() => 
+          handleVote(result.data._id, emojis[i], i)
+        } key={i}>
+          {emojis[i]}: {result.data.votes[i].value}
+        </button>
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleVote = async (jokeId, voteType, emojiNumb) => {
+    const data = { jokeId, voteType };
+
+    try {
+      const result = await axios.post('http://localhost:7070/api/updateVotes/', data);
+      console.log(result.data.votes[emojiNumb].value)
+    } catch (error) {
+      console.error('Error updating vote:', error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <section className={s.fBody}>
@@ -10,13 +56,18 @@ function App() {
         <p>Welcome to RanDay.jokes, where we will publish jokes to make you laugh</p>
       </div>
       <div className={s.mainSection}>
-        <div className={s.jokeBlock}>
-          Why did the baker go to therapy?
-          Because every time he tried to make dough, he just kept kneading validation from everyone around him!
+        <div>
+          <div className={s.jokeBlock}>
+            <div>{header}</div>
+            <div>{response}</div>
+          </div>
+          <div className={s.emojis}>
+            {emojiRes}
+          </div>
         </div>
         <span className={s.blockBtn}>
           <p>Next joke</p>
-          <button className={s.nextBtn}><GrFormNext className={s.iconBtn} /></button>
+          <button onClick={getData} className={s.nextBtn}><GrFormNext className={s.iconBtn} /></button>
         </span>
       </div>
     </section>
